dc56123839bde55f18fe9d89d7e26d4f29515377
barton <18298498823@163.com>
2018-07-08 15:25:18 +0800

    controller

--- src/main/java/com/barton/controller/DashboardController.java
+++ src/main/java/com/barton/controller/DashboardController.java
@@ -2,8 +2,17 @@ package com.barton.controller;
 
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
+import com.barton.service.DietBot;
+import com.barton.service.DietService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Action;
+import java.io.IOException;
 
 /**
  * 项目名称：ssm
@@ -12,14 +21,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DashboardController {
     private static final Log logger = LogFactory.get();
-    @GetMapping(value = {"/dashboard"})
-    public String index() {
-        return "/dashboard/index";
-    }
 
-    @GetMapping(value = {"/main"})
-    public String main() {
-        return "/dashboard/mian/index";
+
+
+    @PostMapping(value = {"/dashboard"})
+    public void index(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 创建Bot
+        DietBot bot = new DietBot(request);
+        // 打开签名验证
+        bot.enableVerify();
+        // 关闭签名验证
+        // bot.disableVerify();
+
+        try {
+            // 调用bot的run方法
+            String responseJson = bot.run();
+            // 设置response的编码UTF-8
+            response.setCharacterEncoding("UTF-8");
+            // 返回response
+            response.getWriter().append(responseJson);
+        } catch (Exception e) {
+            // 返回非法结果
+            response.getWriter().append("{\"status\":1,\"msg\":\"\"}");
+        }
+
     }
 
 }
